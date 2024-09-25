7cda7a126872fd2474bb0c35bebde5c4d07eafa3
Mykola Nosenko <nosenkomi02@gmail.com>
2024-09-25 15:06:09 +0300

    add simple error check

--- app/src/main/java/com/nmi/videotest/presentation/video_list/VideoListScreen.kt
+++ app/src/main/java/com/nmi/videotest/presentation/video_list/VideoListScreen.kt
@@ -14,7 +14,12 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Replay
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -75,41 +80,49 @@ fun VideoListScreen(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                if (state.errorMessage != null) {
+                if (state.isLoading){
+                    CircularProgressIndicator()
+                } else if (state.errorMessage != null) {
                     Text(
                         text = state.errorMessage.toString(),
                         color = MaterialTheme.colorScheme.error
                     )
-                }
-                LazyColumn(
-                    state = lazyListState,
-                    contentPadding = PaddingValues(
-                        horizontal = 0.dp,
-                        vertical = MaterialTheme.spacing.medium
-                    ),
-                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large),
-                ) {
-                    items(state.videos) { video ->
-                        VideoListItem(
-                            imageUrl = video.imageUrl,
-                            id = video.id,
-                            onClick = {
+                    Spacer(modifier = Modifier.size(MaterialTheme.spacing.medium))
+                    IconButton(onClick = { onAction(VideoListAction.Reload) }) {
+                        Icon(imageVector = Icons.Default.Replay, contentDescription = "Reload")
+                    }
+                } else{
+                    LazyColumn(
+                        state = lazyListState,
+                        contentPadding = PaddingValues(
+                            horizontal = 0.dp,
+                            vertical = MaterialTheme.spacing.medium
+                        ),
+                        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large),
+                    ) {
+                        items(state.videos) { video ->
+                            VideoListItem(
+                                imageUrl = video.imageUrl,
+                                id = video.id,
+                                onClick = {
 
-                                Log.d("VideoListScreen", state.videos.indexOf(video).toString())
-                                Log.d(
-                                    "VideoListScreen",
-                                    state.videos.map { it.id }.toList().toString()
-                                )
-                                onAction(
-                                    VideoListAction.GoToVideoPlayer(
-                                        currentVideoPosition = state.videos.indexOf(video),
-                                        videosIds = state.videos.map { it.id }
+                                    Log.d("VideoListScreen", state.videos.indexOf(video).toString())
+                                    Log.d(
+                                        "VideoListScreen",
+                                        state.videos.map { it.id }.toList().toString()
                                     )
-                                )
-                            }
-                        )
+                                    onAction(
+                                        VideoListAction.GoToVideoPlayer(
+                                            currentVideoPosition = state.videos.indexOf(video),
+                                            videosIds = state.videos.map { it.id }
+                                        )
+                                    )
+                                }
+                            )
+                        }
                     }
                 }
+
             }
         }
 
